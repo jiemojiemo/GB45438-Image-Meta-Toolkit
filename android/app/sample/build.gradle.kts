c8c2ff42f3cbd665472ce100b16105c33c4be28c
hw <460706836@qq.com>
2026-02-10 15:10:55 +0800

    try fix jitpack build error

--- android/app/sample/build.gradle.kts
+++ android/app/sample/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
 //    implementation(project(":app:gimt-native"))
     
     // JitPack 测试：取消注释下面这行，注释掉上面的 project 依赖
-    implementation("com.github.jiemojiemo:GB45438-Image-Meta-Toolkit:-SNAPSHOT")
+    implementation("com.github.jiemojiemo:GB45438-Image-Meta-Toolkit:main-SNAPSHOT")
 //    implementation("com.github.jiemojiemo.GB45438-Image-Meta-Toolkit:gimt-native:v1.0.0")
     
     testImplementation(libs.junit)
